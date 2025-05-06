c35ca4e4febb1612aa8b398f926b13656b358f6f
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-05-06 16:25:05 +0000

    created company and contacts pages from databases

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "roll")
+  get("/companies", :controller => "companies", :action => "index")
+  get("/contacts", :controller => "contacts", :action => "index")
   # all the items in "" you come up with yourself
   # when a request comes in to /tacos, you can handle it
 end
